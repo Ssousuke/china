7e8053a645399b4381b1805b03a761673eb12d7b
Wesley Farias <wesleysousukee@gmail.com>
2022-03-06 20:58:28 -0300

    ajustes

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
 from . models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'category',
+        'author',
+        'created_at',
+        'updated_at',
+        'published'
+    ]
+
+    list_display_links = [
+        'title',
+        'category',
+        'author',
+        'created_at',
+        'updated_at'
+    ]
+
+    list_editable = [
+        'published'
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_display_links = [
+        'name',
+        'created_at',
+        'updated_at',
+    ]
