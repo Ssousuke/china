2fe4cf302d5d72a8b7e8fcf665dac6ffdc5b4714
Wesley Farias <wesleysousukee@gmail.com>
2022-03-24 01:15:24 -0300

    Alterações no HTML

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,14 +4,16 @@ from apps.blog.models import Post, Category
 
 def home(request):
     template_name = 'blog/home.html'
+    list_categories = Category.objects.all()
     list = Post.objects.filter(published=True)
-    return render(request, template_name, {'list': list})
+    return render(request, template_name, {'list': list, 'categories': list_categories})
 
 
 def detail(request, slug):
     template_name = 'blog/detail.html'
+    list_categories = Category.objects.all()
     detail = get_object_or_404(Post, published=True, slug=slug)
-    return render(request, template_name, {'detail': detail})
+    return render(request, template_name, {'detail': detail, 'categories': list_categories})
 
 
 def categories(request):
